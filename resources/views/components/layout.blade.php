5cdf0c72ceb52eb94c1075ade8f097620bb7ab13
jp319 <jpantipuesto00104@usep.edu.ph>
2023-07-18 20:52:46 +0800

    Section 9 | User Registration Page is Build

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,11 +2,14 @@
 
 <title>Laravel From Scratch Blog</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-<body style="font-family: Open Sans, sans-serif">
+<body style="font-family: 'Poppins', sans-serif;">
 <section class="px-6 py-8">
     <nav class="md:flex md:justify-between md:items-center">
         <div>
